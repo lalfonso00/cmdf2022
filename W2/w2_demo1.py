129f14965ba087c2b213c71c53de1c69f3e4019f
Loreena Alfonso <loreenaalfonso@gmail.com>
2022-03-05 14:00:16 -0800

    added test.html to provide an example of javascript post and get request Co-authored-by: sk_00000 <SaKura-jp@users.noreply.github.com> Co-authored-by: Zayce <Zayce@users.noreply.github.com>

--- W2/w2_demo1.py
+++ W2/w2_demo1.py
@@ -5,33 +5,43 @@ app = Flask(__name__)
 
 @app.route('/')
 def init():                            # this is a comment. You can create your own function name
-    return '<h1> {} </h1>'.format(__name__)
+    return '<h1> HELLO WORLD </h1>'
 
-@app.route('/about')
+
+@app.route('/test')
 def home():
-    return render_template('about.html',  name='Special Agent 080')  # the html file should be in templates (folder)
+    # the html file should be in templates (folder)
+    return render_template('test.html')
 
-@app.route('/passing')                  # http://3.237.92.38:5000/passing or http://54.243.250.140:5000/passing?dcode=abc
+
+# http://3.237.92.38:5000/passing or http://54.243.250.140:5000/passing?dcode=abc
+@app.route('/passing')
 def show_code():
-    code = request.args.get('dcode','no value given') 
+    code = request.args.get('dcode', 'no value given')
     return code
-    
+
 # info about escape : https://tedboy.github.io/flask/generated/flask.escape.html
 
-@app.route('/user/<username>')          # http://3.237.92.38:5000/user/Jake or http://3.237.92.38:5000/user/Whatever%20Name
+
+# http://3.237.92.38:5000/user/Jake or http://3.237.92.38:5000/user/Whatever%20Name
+@app.route('/user/<username>')
 def show_user(username):
     # show the user profile for that user
     return 'User %s' % escape(username)
+
 
 @app.route('/post/<int:post_id>')       # http://3.237.92.38:5000/post/145
 def show_post(post_id):                 # id is an integer
     num = 888
     return 'Post %d. It is a nice number. I like # %d' % (post_id, num)
 
-@app.route('/path/<path:subpath>')      # http://3.237.92.38:5000/path/folderA/folderB
+
+# http://3.237.92.38:5000/path/folderA/folderB
+@app.route('/path/<path:subpath>')
 def show_subpath(subpath):
     # show the subpath after /path/
     return 'Subpath %s' % escape(subpath)
-    
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
